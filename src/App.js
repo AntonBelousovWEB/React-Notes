09e42013faa43ecd268109f0b6ee353ded64b9e3
Anton <polpoltrop228@gmail.com>
2023-01-07 21:02:24 +0200

    Version 2.3.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import uuid from "react-uuid";
+import './styles/style.css';
+import AppCard from './components/AppCard.jsx';
+var pjson = require('../package.json');
 
 function App() {
+  const [text, setText] = React.useState("")
+
+  const [notes, setNotes] = React.useState([1]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid()
+    }
+    setNotes([newNote, ...notes]);
+    console.log(notes)
+  }
+
+  const onDeleteNote = (noteId) => {
+    setNotes(notes.filter(({ id }) => id !== noteId));
+  }
+
+  const onDeleteText = () => {
+    setText("")
+  }
+
+  const onViewText = () => {
+    if(notes.length === 1) {
+      setText("You haven't Notes :(")
+    }
+    else {
+      setText("")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body_container">
+      <AppCard
+        notes={notes}
+        onAddNote={onAddNote} 
+        onDeleteNote={onDeleteNote} 
+        text={text} 
+        onDeleteText={onDeleteText}
+        onViewText={onViewText}
+        VersionNotes={pjson.version}
+      />
     </div>
   );
 }
